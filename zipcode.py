964e6bb54db69444fb603954d96e33410d947c4e
gwen <gwenael.kreutner1@gmail.com>
2019-05-21 13:42:54 +0200

    Initial commit

---
+++ zipcode.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+import zipfile
+
+
+# Declare the function to return all file paths of the particular directory
+def retrieve_file_paths(dirName):
+    # setup file paths variable
+    filePaths = []
+
+    # Read all directory, subdirectories and file lists
+    for root, directories, files in os.walk(dirName):
+        for filename in files:
+            # Create the full filepath by using os module.
+            filePath = os.path.join(root, filename)
+            filePaths.append(filePath)
+
+    # return all paths
+    return filePaths
+
+
+# Declare the main function
+def zipping(dir_name):
+    # Call the function to retrieve all files and folders of the assigned directory
+    filePaths = retrieve_file_paths(dir_name)
+
+    # printing the list of all files to be zipped
+    print('The following list of files will be zipped:')
+    for fileName in filePaths:
+        print(fileName)
+
+    # writing files to a zipfile
+    zip_file = zipfile.ZipFile(dir_name + '.zip', 'w')
+    with zip_file:
+        # writing each file one by one
+        for file in filePaths:
+            zip_file.write(file)
+    shutil.rmtree(dir_name)
+    print(dir_name + '.zip file is created successfully!')
